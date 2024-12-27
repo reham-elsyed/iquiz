29afa3b64908b7c2430b365c32f4a595143419ca
reham-elsayed <rehamshipl445@gmail.com>
2024-12-27 13:06:22 +0200

    adjust runtime for api question route

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -5,8 +5,8 @@ import { NextResponse } from "next/server";
 import { ZodError } from "zod";
 import axios from "axios";
 
-export const runtime = "nodejs";
-export const maxDuration = 60;
+// export const runtime = "nodejs";
+// export const maxDuration = 60;
 
 export async function POST(req: Request, res: Response) {
   try {
