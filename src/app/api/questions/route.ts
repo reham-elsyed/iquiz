faefb69486995cff6a128973b8c02ad20a09390d
reham-elsayed <rehamshipl445@gmail.com>
2024-12-27 13:01:48 +0200

    adjust runtime for api question route

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -3,13 +3,14 @@ import { getAuthSession } from "@/lib/nextAuth";
 import { quizCreationSchema } from "@/app/schemas/formSchema/quizSchema";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
+import axios from "axios";
 
 export const runtime = "nodejs";
-export const maxDuration = 500;
+export const maxDuration = 60;
 
 export async function POST(req: Request, res: Response) {
   try {
-    const session = await getAuthSession();
+  //  const session = await getAuthSession();
     // if (!session?.user) {
     //   return NextResponse.json(
     //     { error: "You must be logged in to create a game." },
@@ -25,8 +26,8 @@ export async function POST(req: Request, res: Response) {
       questions = await strict_output(
         `You are a professional question creator that generate a pair of question and answers, the length of each answer should not be more than 15 words, store all the pairs of answers and questions in a JSON array You are to generate exactly ${amount} open-ended questions about ${topic}`,type
         )  
-    }
-     else if (type === "mcq") {
+    }  
+else if (type === "mcq") {
       questions = await strict_output(
         `You are a expert question designer and interviewer that is able to generate exactly ${amount} mcq questions and answers about ${topic}, the length of each answer should not be more than 15 words always return one right answer in the choices`,type
       );
