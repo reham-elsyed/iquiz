88e6730eb6c2d411e0f0d04bd5c15fe570dbfc62
reham-elsayed <rehamshipl445@gmail.com>
2025-10-09 22:37:20 +0300

    login page video illustration

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -23,8 +23,20 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
       <div className="flex items-center justify-center h-screen overflow-hidden ">
         <div className="container  mx-auto p-5 rounded-2xl  ">
           <div className="grid grid-cols-1 lg:grid-cols-7  relative ">
-            <div className="hidden lg:block  lg:col-span-4  ">
-              <LoginComponent /></div>
+            <div className="hidden lg:block  lg:col-span-4 relative ">
+
+              <div
+                className="absolute -top-20 -left-30 h-[130%] rounded-2xl border-4 border-amber-500 w-[100%] aspect-video  mx-auto">
+                <video
+                  loop
+                  autoPlay
+                  muted
+                  playsInline
+                  src="https://gokxczesysklzepbknzb.supabase.co/storage/v1/object/sign/demo%20video/Untitled%20design%20(2).mp4?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hMGRlYWQwYy1kNzY4LTQwMTEtYTkwNS0wNWM0M2JhZDY1MjIiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJkZW1vIHZpZGVvL1VudGl0bGVkIGRlc2lnbiAoMikubXA0IiwiaWF0IjoxNzYwMDIzODY3LCJleHAiOjE5MTc3MDM4Njd9.qIDk9HqG5Le_Z1VutGTe28P-LEyK8vGBjMdSjge6rP0"
+                  controls
+                  className="w-full h-full object-fill rounded-xl shadow-lg" />
+              </div>
+            </div>
             <div className=" relative flex  flex-col  md:items-center md:justify-center col-span-1 lg:col-span-3 ">
               {children}
             </div>
