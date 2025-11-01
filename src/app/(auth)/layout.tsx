bfbab79c27e1da173f1ef5cbf46649163f690bf7
reham-elsayed <rehamshipl445@gmail.com>
2025-11-01 16:35:04 +0200

    update width build

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,9 +6,9 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
     <>
       <div className="flex items-center justify-center h-screen overflow-hidden relative">
         <AnimatedGrid />
-        <div className="container  mx-auto px-5  ">
-          <div className="grid grid-cols-1 lg:grid-cols-7  relative ">
-            <div className=" relative flex  flex-col  md:items-center md:justify-center col-span-1 lg:col-span-3 lg:col-start-3 lg:col-end-6 rounded-2xl backdrop-blur-xl bg-white/10 border border-white/20 shadow-[0_0_40px_-10px_rgba(0,0,0,0.6)] ">
+        <div className="container  mx-auto px-5">
+          <div className="grid grid-cols-1 lg:grid-cols-7  relative">
+            <div className=" relative flex  flex-col  md:items-center md:justify-center col-span-1 lg:col-span-3 lg:col-start-3 lg:col-end-6 rounded-2xl backdrop-blur-xl bg-white/10 border border-white/20 shadow-[0_0_40px_-10px_rgba(0,0,0,0.6)]">
               {children}
             </div>
           </div>
